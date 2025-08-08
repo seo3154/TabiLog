cf28106e8ea050679cb2bb29fab8cd5749bda488
jk9593 <jk9593@gmail.com>
2025-08-08 18:06:00 +0900

    index.js에서 css파일 이름수정
    index.js에서 css파일 이름수정

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./styles/NoticeDetail.css";
+import "./styles/Notice.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
